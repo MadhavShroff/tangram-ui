9d9e144a03c51bc364190577ad878d014a5eb966
MadhavShroff <madhavshroff99@gmail.com>
2025-03-04 20:11:50 -0500

    add strategy page

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
